e5da141d1e1068995ba73dde3d74f75aa1da2cf5
Pavankumar13268 <chadarampavankumar8@gmail.com>
2025-08-24 17:45:35 +0530

    dot net core project move

---
+++ CodePluse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePluse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BlogImage> BlogImages { get; set; }
+    }
+}
